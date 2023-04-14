0fd5aba73077d9358ce8e328e4a79890fac2a181
manuelurruti <manuelurrutimarquez@gmail.com>
2023-04-14 06:06:48 -0300

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
     return (
         <div>
             <header>
-                <h1 className='title'> <a href="/" >Peliculas </a></h1>
+                <h1 className='title'> <a href="/" >Films </a></h1>
             </header>
             <MyRoutes />
         </div>
